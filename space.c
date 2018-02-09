3015037af2f41537c0c3f9310545bf5fa7ca42c6
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-02-09 15:07:39 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char c[i],space=0;
+    for(i=0;i<=0;i++')
+    {
+        if(a[i]==" ")
+        {
+            space++;
+        }
+        printf("%d",space");
+        }
+    }
+   } 
